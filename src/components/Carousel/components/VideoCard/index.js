7082375b1b4aa2536b2eaacb1cbe9c87b2a0ecbb
William Goncalves <euwilliamgoncalves@gmail.com>
2020-07-27 19:22:14 -0300

    Removendo comentários do index do card de vídeo

--- src/components/Carousel/components/VideoCard/index.js
+++ src/components/Carousel/components/VideoCard/index.js
@@ -20,15 +20,15 @@ function VideoCard({ videoTitle, videoURL, categoryColor }) {
       target="_blank"
       style={{ borderColor: categoryColor || 'red' }}
       title={videoTitle}
-    > {/* Abrir a tag do componente atual */}
+    >
 
       <VideoCardBanner style={{ backgroundColor: categoryColor || 'red' }}> 
-      {/* abrir novo componente usando cor da categoria como background */}
-
-            {videoTitle} {/* Título do vídeo como texto dentro da <p> */}
       
-      </VideoCardBanner> {/* Fechar novo componente */}
-    </VideoCardContainer> {/* Fechar componente atual */}
+
+            {videoTitle} 
+      
+      </VideoCardBanner> 
+    </VideoCardContainer>
   );
 }
 
