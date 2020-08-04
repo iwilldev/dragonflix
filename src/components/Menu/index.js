0c1cb0907a750cb7bc0c9e764a0adc19f5fa1062
William Goncalves <euwilliamgoncalves@gmail.com>
2020-08-03 22:58:29 -0300

    Botão de menu dinâmico

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -5,14 +5,28 @@ import ButtonLink from './ButtonLink';
 import './Menu.css';
 
 function Menu() {
-    return (
-        <nav className="Menu">
-            <Link to="/">
-                <img className="Logo" src={Logo} alt="Logo Dragonflix" />
-            </Link>
-            <ButtonLink as={Link} className="ButtonLink" to="/cadastro/video">Novo Vídeo</ButtonLink>
-        </nav>
-    )
+  let buttonRoute = '';
+  let buttonText = '';
+
+  if (window.location.href.includes('/video')) {
+    buttonRoute = '/cadastro/categoria';
+    buttonText = 'Nova Categoria';
+  } else if (window.location.href.includes('/categoria') || (window.location.href.includes('/watch'))) {
+    buttonRoute = '/';
+    buttonText = 'Página Inicial';
+  } else {
+    buttonRoute = '/cadastro/video';
+    buttonText = 'Novo Vídeo';
+  }
+
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="Logo Dragonflix" />
+      </Link>
+      <ButtonLink as={Link} className="ButtonLink" to={buttonRoute}>{buttonText}</ButtonLink>
+    </nav>
+  );
 }
 
 export default Menu;
