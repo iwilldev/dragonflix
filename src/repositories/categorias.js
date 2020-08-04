9601cbe43d8a496f1474e6c129743123b2be1bf3
William Goncalves <euwilliamgoncalves@gmail.com>
2020-08-03 22:57:20 -0300

    Incluindo repositórios de video e categoria

--- src/repositories/categorias.js
+++ src/repositories/categorias.js
@@ -1,15 +1,32 @@
 import config from '../config';
 
-const URL_CATEGORIAS = `${config.URL_BACKEND_TOP}/categorias`;
+const URL_CATEGORIES = `${config.URL_BACKEND_TOP}/categorias`;
+
+function getAll() {
+  return fetch(`${URL_CATEGORIES}`)
+    .then(async (respostaDoServidor) => {
+      if (respostaDoServidor.ok) {
+        const resposta = await respostaDoServidor.json();
+        return resposta;
+      }
+
+      throw new Error('Não foi possível pegar os dados :(');
+    });
+}
 
 function getAllWithVideos() {
-  return fetch(URL_CATEGORIAS)
+  return fetch(`${URL_CATEGORIES}?_embed=videos`)
     .then(async (respostaDoServidor) => {
-      const resposta = await respostaDoServidor.json();
-      return resposta;
+      if (respostaDoServidor.ok) {
+        const resposta = await respostaDoServidor.json();
+        return resposta;
+      }
+
+      throw new Error('Não foi possível pegar os dados :(');
     });
 }
 
 export default {
   getAllWithVideos,
+  getAll,
 };
